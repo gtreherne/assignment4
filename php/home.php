3a0be97c6da6d00524f3f4e40c883baf03f377f2
graeme <graeme@shift.org.nz>
2017-09-22 11:28:26 +1200

    site structure set up - home page and css

---
+++ php/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+	<link rel="stylesheet" type="text/css" href="../css/style">
+</head>
+<body>
+<header>
+	<h1>COMP233 Multiple Choice Tests</h1>
+</header>
+You can view tests but not take them until you log in
+<nav>
+	<ul>
+		<li><a href="login.php">Log in</a></li>
+		<li><a href="test1.php">Test #1</a></li>
+		<li><a href="test2.php">Test #2</a></li>
+		<li><a href="test3.php">Test #3</a></li>
+	</ul>
+</nav>
+</body>
+</html>
